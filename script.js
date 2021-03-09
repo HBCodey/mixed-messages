dc22d72a9d4dc3021843dcff129d932fb8077c56
Helle <79770005+HBCodey@users.noreply.github.com>
2021-03-09 09:45:50 +0000

    wrote all the quotes, characters and movies/series, dont want to have to do that again, so committing!

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 // Movie Quote Generator
+
+const quoteArray = [
+    "For the time will soon come when Hobbits will shape the fortunes of all.",
+    "I would rather share one lifetime with you than face all the Ages of this world alone.",
+    "You shall not pass!",
+    "That there’s some good in this world, Mr. Frodo… and it’s worth fighting for.",
+    "Yer a wizard Harry.",
+    "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+    "I solemnly swear I am up to no good.",
+    "You’re just as sane as I am.",
+    "Winter is coming.",
+    "You know nothing, Jon Snow.",
+    "A lion doesn't concern himself with the opinions of sheep.",
+    "When you play the game of thrones, you win or you die",
+    "Never forget what you are. The rest of the world will not. Wear it like armour, and it can never be used to hurt you."
+];
+
+const characterArray = [
+    "Galadriel",
+    "Arwen",
+    "Gandalf",
+    "Sam",
+    "Hagrid",
+    "Dumbledore",
+    "Harry",
+    "Luna",
+    "Ned",
+    "Ygritte",
+    "Tywin",
+    "Cersei",
+    "Tyrion"
+];
+
+const movieArray = [
+    "The Lord of the Rings: The Fellowship of the Ring",
+    "The Lord of the Rings: The Two Towers",
+    "The Lord of the Rings: Return of the King",
+    "Harry Potter and the Philosophers Stone",
+    "Harry Potter and the Chamber of Secrets",
+    "Harry Potter and the Prizoner of Azkaban",
+    "Harry Potter and the Order of the Phoenix",
+    "A Game of Thrones (TV Series)"
+];
+

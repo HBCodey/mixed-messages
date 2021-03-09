92f6d0777029393d0079f6e925d1c7eba8cdaf94
Helle <79770005+HBCodey@users.noreply.github.com>
2021-03-09 10:39:12 +0000

    moved everything inside the update function so that the button would rerun the whole script every time and keep creating new quotes

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 // Movie Quote Generator
 
-//Quotes 
-const quoteArray = [
-    "For the time will soon come when Hobbits will shape the fortunes of all.",
-    "I would rather share one lifetime with you than face all the Ages of this world alone.",
-    "You shall not pass!",
-    "That there’s some good in this world, Mr. Frodo… and it’s worth fighting for.",
-    "Yer a wizard Harry.",
-    "It is our choices, Harry, that show what we truly are, far more than our abilities.",
-    "I solemnly swear I am up to no good.",
-    "You’re just as sane as I am.",
-    "Winter is coming.",
-    "You know nothing, Jon Snow.",
-    "A lion doesn't concern himself with the opinions of sheep.",
-    "When you play the game of thrones, you win or you die",
-    "Never forget what you are. The rest of the world will not. Wear it like armour, and it can never be used to hurt you."
-];
-
+const randomize = arr => {
+    let randomNum = Math.floor(Math.random()*(arr.length));
+    return arr[randomNum];
+};
+const update = () => {
 //Characters
 const characterArray = [
     "Galadriel",
@@ -33,6 +21,27 @@ const characterArray = [
     "Cersei",
     "Tyrion"
 ];
+let randomCharacter1 = randomize(characterArray);
+let randomCharacter2 = randomize(characterArray);
+
+//Quotes 
+const quoteArray = [
+    "For the time will soon come when Hobbits will shape the fortunes of all.",
+    "I would rather share one lifetime with you than face all the Ages of this world alone.",
+    "You shall not pass!",
+    `That there’s some good in this world, Mr. ${randomCharacter2}… and it’s worth fighting for.`,
+    `Yer a wizard ${randomCharacter2}.`,
+    `It is our choices, ${randomCharacter2}, that show what we truly are, far more than our abilities.`,
+    "I solemnly swear I am up to no good.",
+    "You’re just as sane as I am.",
+    "Winter is coming.",
+    `You know nothing, ${randomCharacter2} Snow.`,
+    "A lion doesn't concern himself with the opinions of sheep.",
+    "When you play the game of thrones, you win or you die",
+    "Never forget what you are. The rest of the world will not. Wear it like armour, and it can never be used to hurt you."
+];
+
+let randomQuote = randomize(quoteArray);
 
 //Movies / TV series
 const movieArray = [
@@ -46,19 +55,23 @@ const movieArray = [
     "A Game of Thrones (TV Series)"
 ];
 
-
-const randomize = arr => {
-    let randomNum = Math.floor(Math.random()*(arr.length));
-    return arr[randomNum];
-};
-
-let randomQuote = randomize(quoteArray);
-let randomCharacter = randomize(characterArray);
 let randomMovie= randomize(movieArray);
 
+//insert into HTML
+
+document.getElementById("quote").innerHTML = '"' + randomQuote + '"';
+document.getElementById("character").innerHTML = "-" + randomCharacter1;
+document.getElementById("movie").innerHTML= randomMovie;
 
 //The below is for running only in JavaScript. I will however integrate this in HTML to make it look pretty
-let output = `"${randomQuote}" \n \n -${randomCharacter} \n ${randomMovie}`;
+let output = `"${randomQuote}" \n \n -${randomCharacter1} \n ${randomMovie}`;
 
-console.log(output);
+console.log(output)};
+
+document.getElementById("myButton").addEventListener("click", update);
+
+
+
+
+
 
